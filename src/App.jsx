c2a08f9686815a1a4119aa6c2423642d7dad9311
Chris Candelora <chris@inigus.com>
2023-08-01 13:59:49 -0400

    adding facebook

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   createBrowserRouter,
   Router,
@@ -39,6 +39,38 @@ function App() {
       element: <WaitTimes title="Aniaml Kingdom Wait Times" park="animal-kingdom"/>
     },
   ]);
+
+  const initFacebookSDK = () => {
+    if (window.FB) {
+      window.FB.XFBML.parse();
+    }
+    let locale = "vi_VN";
+    window.fbAsyncInit = function () {
+      window.FB.init({
+        appId: "2058140294535510",
+        cookie: true, // enable cookies to allow the server to access
+        // the session
+        xfbml: true, // parse social plugins on this page
+        version: "v2.5" // use version 2.1
+      });
+    };
+    // Load the SDK asynchronously
+    (function (d, s, id) {
+      console.log(s);
+      var js,
+        fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = `//connect.facebook.net/${locale}/sdk.js`;
+      fjs.parentNode.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  };
+
+  useEffect(() => {
+    initFacebookSDK();
+  }, []);
+
   return (
     <div>
        
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -21,34 +21,7 @@ function Post(props) {
       year: "numeric",
     }
   );
-  const initFacebookSDK = () => {
-    if (window.FB) {
-      window.FB.XFBML.parse();
-    }
-    let locale = "vi_VN";
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: "2058140294535510",
-        cookie: true, // enable cookies to allow the server to access
-        // the session
-        xfbml: true, // parse social plugins on this page
-        version: "v2.5" // use version 2.1
-      });
-    };
-    // Load the SDK asynchronously
-    (function (d, s, id) {
-      console.log(s);
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = `//connect.facebook.net/${locale}/sdk.js`;
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  };
-  console.log(post);
-
+  
   useEffect(() => {
     const postApiEndPoint =
       "https://oyster-app-sus4c.ondigitalocean.app/post/" + props.slug;
