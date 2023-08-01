9254035fc62eda8c82dd286050b3ce11a075475c
Chris Candelora <chris@inigus.com>
2023-08-01 15:15:35 -0400

    facebook comment

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import {
   createBrowserRouter,
   Router,
   RouterProvider,
 } from "react-router-dom";
 import WaitTimes from './components/WaitTimes';
-import Posts from './components/Posts';
 import PostWraper from './components/PostWraper';
 import PostsWraper from './components/PostsWraper';
 import HomeWraper from './components/HomeWraper';
-import Post from './components/Post';
 import Header from './components/Header';
-import Test from './components/Test';
 import Footer from './components/Footer';
 
 function App() {
@@ -40,37 +37,7 @@ function App() {
     },
   ]);
 
-  const initFacebookSDK = () => {
-    if (window.FB) {
-      window.FB.XFBML.parse();
-    }
-    let locale = "vi_VN";
-    window.fbAsyncInit = function () {
-      window.FB.init({
-        appId: "2058140294535510",
-        cookie: true, // enable cookies to allow the server to access
-        // the session
-        xfbml: true, // parse social plugins on this page
-        version: "v2.5" // use version 2.1
-      });
-    };
-    // Load the SDK asynchronously
-    (function (d, s, id) {
-      console.log(s);
-      var js,
-        fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return;
-      js = d.createElement(s);
-      js.id = id;
-      js.src = `//connect.facebook.net/${locale}/sdk.js`;
-      fjs.parentNode.insertBefore(js, fjs);
-    })(document, "script", "facebook-jssdk");
-  };
-
-  useEffect(() => {
-    initFacebookSDK();
-  }, []);
-
+  
   return (
     <div>
        
