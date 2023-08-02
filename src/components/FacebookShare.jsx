c0706e8cf4c2a01f3f1d111bd962db9803a409da
Chris Candelora <chris@inigus.com>
2023-08-01 23:07:47 -0400

    fixing props in facebook like and share

--- src/components/FacebookShare.jsx
+++ src/components/FacebookShare.jsx
@@ -2,13 +2,18 @@ import React from "react";
 
 export default function FacebookShare(props) {
     let { link } = props;
+    const url = link + "&src=sdkpreparse";
+    const baseFacebookUrl = "https://www.facebook.com/sharer/sharer.php?u=";
+    const facebookUrl = baseFacebookUrl + encodeURI(url);
     return (
       <>
         <div
           className="fb-share-button"
           data-href={link}
           data-layout="button_count"
-        ></div>
+          data-size="small"
+        >
+        </div>
       </>
     );
   }
